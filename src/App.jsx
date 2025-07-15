c32c495655f2d14b962f8f971e331fa680d22fcd
thelegend2004 <oleh.liubichenko@gmail.com>
2025-07-15 12:47:08 +0300

    feature: add new names. move files to proper folders. add new images. change styles. add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import spanishNames from "./data/names.json";
 
-const spanishNames = ["Pepe", "Lola", "Cipriano", "Bruno"];
+const images = import.meta.glob("./assets/animals/*.{jpg,jpeg,png,gif}", {
+  eager: true,
+  import: "default",
+  query: { url: true },
+});
 
-const animalImages = [
-  "/animals/1.jpg",
-  "/animals/2.jpg",
-  "/animals/3.jpg",
-  "/animals/4.jpg",
-];
+const animalImages = Object.values(images);
 
 function App() {
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(
+    Math.floor(Math.random() * animalImages.length)
+  );
 
   const generateNew = () => {
     const randomIndex = Math.floor(Math.random() * animalImages.length);
@@ -22,19 +24,24 @@ function App() {
 
   return (
     <div className="text-center p-5">
-      <h1>Animal Stuck in a Funny Place Generator!</h1>
+      <h1 className="mb-4 text-2xl font-semibold">
+        Animal Stuck in a Funny Place Generator!
+      </h1>
       <div className="relative inline-block">
         <img
           src={animalImages[index]}
           alt="Animal stuck in a funny and weird place"
-          className="w-[400px] h-[500px] rounded-[12px]"
+          className="w-[500px] h-[500px] rounded-[12px]"
         />
         <div className="absolute bottom-5 left-0 right-0 text-white text-4xl font-bold text-shadow-black">
           {randomName}
         </div>
       </div>
       <br />
-      <button onClick={generateNew} className="mt-5">
+      <button
+        onClick={generateNew}
+        className="mt-5 bg-amber-400 p-4 rounded-xl font-semibold cursor-pointer hover:p-5 hover:bg-amber-600"
+      >
         Generate Another
       </button>
     </div>
